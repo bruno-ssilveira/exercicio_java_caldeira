dd2125c2201507e30b6d65951893a801c201f44e
bruno-ssilveira <129340450+bruno-ssilveira@users.noreply.github.com>
2023-09-06 14:51:36 -0300

    exercicio02

---
+++ aula01/exercicio02.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class exercicio02 {
+
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número abaixo: ");
+
+        int n1 = leitor.nextInt();
+
+        System.out.println("Digite o segundo número abaixo: ");
+
+        int n2 = leitor.nextInt();
+
+        System.out.println("Primeiro número: " + n1 + " Segundo número: " + n2);
+
+    }
+
+}
