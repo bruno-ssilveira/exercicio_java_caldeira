cb83defae24cb826aa8ece81ee4b6942fefd33ad
bruno-ssilveira <129340450+bruno-ssilveira@users.noreply.github.com>
2023-09-06 14:29:10 -0300

    exercicio01

---
+++ aula01/exercicio01.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class exercicio01 {
+
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite seu nome abaixo: ");
+
+        String nomeDigitado = leitor.nextLine();
+
+        System.out.println("Nome: " + nomeDigitado);
+
+    }
+
+}
