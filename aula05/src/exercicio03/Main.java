da41e0700e767b93abfdb9dbf65d49cbe6f4b5c4
aluno <aluno@MacBook-Pro-de-Administrador-2.local>
2023-10-03 16:36:17 -0300

    ˜att˜

--- aula05/src/exercicio03/Main.java
+++ aula05/src/exercicio03/Main.java
@@ -25,25 +25,28 @@ public class Main {
                     """);
             String opcao = leitor.nextLine();
 
-            if (opcao.equals("a")) {
-                System.out.println("Digite o nome da tarefa:");
-                String nomeTarefa = leitor.nextLine();
-
-                System.out.println("Digite a data para a tarefa:");
-                String dataTarefa = leitor.nextLine();
-
-                System.out.println("Digite a descrição para a tarefa");
-                String descricaoTarefa = leitor.nextLine();
-
-                System.out.println("Tarefa adicionada!");
-
-                listaTarefas.add("Tarefa: " + nomeTarefa + "\nData: " + dataTarefa + "\nDescrição: " + descricaoTarefa);
-            } else if (opcao.equals("b")) {
-                for (String tarefas : listaTarefas) {
-                    System.out.println(tarefas);
+            switch (opcao) {
+                case "a" -> {
+                    System.out.println("Digite o nome da tarefa:");
+                    String nomeTarefa = leitor.nextLine();
+                    System.out.println("Digite a data para a tarefa:");
+                    String dataTarefa = leitor.nextLine();
+                    System.out.println("Digite a descrição para a tarefa");
+                    String descricaoTarefa = leitor.nextLine();
+                    System.out.println("Tarefa adicionada!");
+                    listaTarefas.add("Tarefa: " + nomeTarefa + "\nData: " + dataTarefa + "\nDescrição: " + descricaoTarefa);
                 }
-            } else if (opcao.equals("d")){
-                loop = false;
+                case "b" -> {
+                    for (String tarefas : listaTarefas) {
+                        System.out.println(tarefas);
+                    }
+                }
+                case "c" -> {
+                    System.out.println("Digite entre aspas o nome da tarefa que deseja remover:");
+                    String remova = leitor.nextLine();
+                    listaTarefas.remove(remova);
+                }
+                case "d" -> loop = false;
             }
         }
     }
