b3301dd5492e271f204c9a8d52b766e88d32ec9e
bruno-ssilveira <129340450+bruno-ssilveira@users.noreply.github.com>
2023-10-27 13:53:41 -0300

    exercicios aula 07

--- aula07/src/exercicio04/Calculadora.java
+++ aula07/src/exercicio04/Calculadora.java
@@ -1,25 +1,123 @@
 package exercicio04;
 
+import java.util.Scanner;
+
 public class Calculadora implements Calculavel {
 
+    enum Operacao {
+        SOMAR,
+        SUBTRAIR,
+        DIVIDIR,
+        MULTIPLICAR
+    }
+
+    public void calcular() {
+        Scanner leitor = new Scanner(System.in);
+
+        boolean play = true;
+
+        while (play) {
+            System.out.println("""
+                    CALCULADORA
+                                    
+                    Digite o calculo desejado: [somar, subtrair, multiplicar, dividir ou sair para finalizar.]
+                    
+                    """);
+            String tipoCalculo = leitor.nextLine().toUpperCase();
+
+            if (tipoCalculo.equals(Operacao.SOMAR.toString())) {
+                somar();
+            } else if (tipoCalculo.equals(Operacao.SUBTRAIR.toString())) {
+                System.out.println("Primeiro número: ");
+                int n1 = leitor.nextInt();
+                System.out.println("Segundo número: ");
+                int n2 = leitor.nextInt();
+
+                int subtracao = n1 - n2;
+                System.out.println("A subtração entre " + n1 + " e " + n2 + " é " + subtracao);
+
+            } else if (tipoCalculo.equals(Operacao.MULTIPLICAR.toString())) {
+                System.out.println("Primeiro número: ");
+                int n1 = leitor.nextInt();
+                System.out.println("Segundo número: ");
+                int n2 = leitor.nextInt();
+
+                int multiplicacao = n1 * n2;
+                System.out.println("A multiplicação entre " + n1 + " e " + n2 + " é " + multiplicacao);
+
+            } else if (tipoCalculo.equals(Operacao.DIVIDIR.toString())) {
+                System.out.println("Primeiro número: ");
+                int n1 = leitor.nextInt();
+                System.out.println("Segundo número: ");
+                int n2 = leitor.nextInt();
+
+                int divisao = n1 / n2;
+                System.out.println("A divisão entre " + n1 + " e " + n2 + " é " + divisao);
+            } else if (tipoCalculo.equals("SAIR")) {
+                play = false;
+            }
+        }
+    }
 
     @Override
     public void somar() {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Primeiro número: ");
+        int n1 = leitor.nextInt();
+        System.out.println("Segundo número: ");
+        int n2 = leitor.nextInt();
 
+        try {
+            if (n1 > 0 && n2 > 0) {
+                int soma = n1 + n2;
+                System.out.println("A soma entre " + n1 + " e " + n2 + " é " + soma);
+            } else {
+                throw new IllegalArgumentException("Número digitado não é inteiro.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
     }
 
     @Override
     public void subtrair() {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Primeiro número: ");
+        int n1 = leitor.nextInt();
+        System.out.println("Segundo número: ");
+        int n2 = leitor.nextInt();
 
+        int subtracao = n1 - n2;
+        System.out.println("A subtração entre " + n1 + " e " + n2 + " é " + subtracao);
+
+        leitor.close();
     }
 
     @Override
     public void multiplicar() {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Primeiro número: ");
+        int n1 = leitor.nextInt();
+        System.out.println("Segundo número: ");
+        int n2 = leitor.nextInt();
 
+        int multiplicacao = n1 * n2;
+        System.out.println("A multiplicação entre " + n1 + " e " + n2 + " é " + multiplicacao);
+
+        leitor.close();
     }
 
     @Override
     public void dividir() {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Primeiro número: ");
+        int n1 = leitor.nextInt();
+        System.out.println("Segundo número: ");
+        int n2 = leitor.nextInt();
 
+        int divisao = n1 / n2;
+        System.out.println("A divisão entre " + n1 + " e " + n2 + " é " + divisao);
+
+        leitor.close();
     }
 }
--- aula07/src/exercicio04/Main.java
+++ aula07/src/exercicio04/Main.java
@@ -5,26 +5,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
-        try {
-            System.out.println("Insira um número inteiro: ");
-            int n1 = leitor.nextInt();
-            System.out.println("Insira um número inteiro: ");
-            int n2 = leitor.nextInt();
+        Calculadora calculo = new Calculadora();
 
-            if (n1 <= 0) {
-                throw new IllegalArgumentException("Número digitado não é inteiro.");
-            } else if (n2 <= 0) {
-                throw new IllegalArgumentException("Número digitado não é inteiro.");
-            }
-
-            if (n1 > 0 && n2 > 0) {
-                System.out.println("Inteiro " + n1 + ", Inteiro " + n2);
-            }
-        } catch (IllegalArgumentException e) {
-            System.err.println("Erro: " + e.getMessage());
-        }
-
+        calculo.calcular();
     }
 
 }
